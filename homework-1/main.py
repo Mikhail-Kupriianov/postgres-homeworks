55315c26f52e1cd7c87070f3e8573383219d35c9
Mkupr <mkupr@outlook.com>
2023-05-04 01:05:36 +0300

    edit main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,8 +8,9 @@ PASS = os.environ.get('PG_PASS')
 DATA_PATH = os.path.abspath("../homework-1/north_data")
 
 
+# Создаём список кортежей из файла csv для передачи в метод executemany
+# is_id_data флаг для добавления id в таблицу employees
 def get_data(is_id_data=False, file_name=""):
-
     result = []
     if os.path.exists(os.path.join(DATA_PATH, file_name)):
         with open(os.path.join(DATA_PATH, file_name), mode="r", encoding='utf-8') as r_file:
@@ -25,13 +26,15 @@ def get_data(is_id_data=False, file_name=""):
     return result
 
 
+# Подключаемся к базе и заполняем таблицы из csv файлов.
 conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=PASS)
 try:
     with conn:
         with conn.cursor() as cur:
             cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", get_data(False, 'customers_data.csv'))
         with conn.cursor() as cur:
-            cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", get_data(True, 'employees_data.csv'))
+            cur.executemany("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                            get_data(True, 'employees_data.csv'))
         with conn.cursor() as cur:
             cur.executemany("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", get_data(False, 'orders_data.csv'))
         with conn.cursor() as cur:
